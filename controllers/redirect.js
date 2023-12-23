171fd5dc3012668409b02a5afa8940ab4faad6ad
Atharvashirsh Tiwary <121449422+Atharvashirsh@users.noreply.github.com>
2023-12-23 16:20:20 +0530

    Added comment on code

--- controllers/redirect.js
+++ controllers/redirect.js
@@ -1,6 +1,7 @@
 //* Require the URL Model(i.e our database)
 const URL = require("../models/url");
 
+//* Create a redirection function
 async function redirect(req, res) {
     const shortid = req.params.shortid;
     const entry = await URL.findOneAndUpdate({ shortId: shortid }, { $push: { visitHistory: { timestamp: Date.now() } } });
